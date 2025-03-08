e8b62a7c3b4962fd9287e6cc18bcced1b5dbe212
Oleg Suroegin <suroegin.oleg.07122004@gmail.com>
2025-03-08 09:20:32 +0300

    add some variables Lesson_1:Task_1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Console.WriteLine("Hello, World!");
+
+int a = 100;
+double b = 1.345;
+char c = '!';
+bool d = true;
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
